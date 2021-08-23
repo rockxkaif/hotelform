330766a2ba25a1dab64654ad3d8bae699a331908
rockxkaif <kaifakhtar121@gmail.com>
2021-08-23 10:27:42 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import Card from "./Components/Card";
 import Footer from "./Components/Footer";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from "./Components/Form";
 
 function App() {
